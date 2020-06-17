fae1481955c450c45e9152ea259366f960f64155
stvnc <vincentangelo96@yahoo.com>
2020-06-17 18:34:25 +0700

    Initial Commit

---
+++ TinderClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinderClone
+//
+//  Created by Vincent Angelo on 17/06/20.
+//  Copyright © 2020 Vincent Angelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
